058d8c72dc1f02cba743bcea6f6a294a44822b21
Glen Merto <strrev4034nelg3441@gmail.com>
2018-09-19 07:52:44 +0800

    Initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+
+<nav class="navbar navbar-expand-xl navbar-dark bg-custom fixed-top">
+    <div class="container">
+        <a class="navbar-brand navTxt" href="index.php">Website Name</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link navTxt" href="#">About Us</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link navTxt" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Curation
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item navTxt" href="#">Weekly</a>
+                        <a class="dropdown-item navTxt" href="#">Monthly</a>
+                        <a class="dropdown-item navTxt" href="#">Yearly</a>
+                        <a class="dropdown-item navTxt" href="#">All Time</a>
+                    </div>
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include "includes/header.php"; ?>
+
+    <!-- Navigation -->
+    <?php include "includes/navbar.php"; ?>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+        
+          <div class="carousel-item active" style="background-image: url('images/david-marcu-114194-unsplash.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Lorem Ipsum</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat condimentum lectus pretium eleifend. Maecenas vel congue justo, at faucibus erat. Aliquam ut interdum leo. In ut eros diam. Proin mattis, arcu eu luctus imperdiet, turpis nisl faucibus diam, nec consectetur nunc nibh in odio. Ut metus lacus, ultricies eu tellus et, scelerisque porttitor ipsum. Curabitur interdum ante eu lacus consequat bibendum. Praesent et augue eget neque dictum viverra. Aliquam pharetra finibus interdum. Suspendisse hendrerit mi ac risus semper fermentum. Quisque sodales rutrum ullamcorper. Praesent vel leo dignissim, vehicula mauris vitae, iaculis ante. Pellentesque dictum quis massa sit amet porta. Proin quis nisi finibus, vulputate tortor commodo, dignissim lorem. Nunc imperdiet ut odio id placerat.</p>
+            </div>
+          </div>
+          <!-- Slide Two - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('images/joep-cox-485712-unsplash.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Lorem Ipsum</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat condimentum lectus pretium eleifend. Maecenas vel congue justo, at faucibus erat. Aliquam ut interdum leo. In ut eros diam. Proin mattis, arcu eu luctus imperdiet, turpis nisl faucibus diam, nec consectetur nunc nibh in odio. Ut metus lacus, ultricies eu tellus et, scelerisque porttitor ipsum. Curabitur interdum ante eu lacus consequat bibendum. Praesent et augue eget neque dictum viverra. Aliquam pharetra finibus interdum. Suspendisse hendrerit mi ac risus semper fermentum. Quisque sodales rutrum ullamcorper. Praesent vel leo dignissim, vehicula mauris vitae, iaculis ante. Pellentesque dictum quis massa sit amet porta. Proin quis nisi finibus, vulputate tortor commodo, dignissim lorem. Nunc imperdiet ut odio id placerat.</p>
+            </div>
+          </div>
+          <!-- Slide Three - Set the background image for this slide in the line below -->
+          <div class="carousel-item" style="background-image: url('images/luis-poletti-17300-unsplash.jpg')">
+            <div class="carousel-caption d-none d-md-block">
+              <h3>Lorem Ipsum</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam volutpat condimentum lectus pretium eleifend. Maecenas vel congue justo, at faucibus erat. Aliquam ut interdum leo. In ut eros diam. Proin mattis, arcu eu luctus imperdiet, turpis nisl faucibus diam, nec consectetur nunc nibh in odio. Ut metus lacus, ultricies eu tellus et, scelerisque porttitor ipsum. Curabitur interdum ante eu lacus consequat bibendum. Praesent et augue eget neque dictum viverra. Aliquam pharetra finibus interdum. Suspendisse hendrerit mi ac risus semper fermentum. Quisque sodales rutrum ullamcorper. Praesent vel leo dignissim, vehicula mauris vitae, iaculis ante. Pellentesque dictum quis massa sit amet porta. Proin quis nisi finibus, vulputate tortor commodo, dignissim lorem. Nunc imperdiet ut odio id placerat.</p>
+            </div>
+          </div>
+        </div>
+        
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </header>
+
+
+<?php include "includes/footer.php"; ?>
