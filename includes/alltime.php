91457873432f09178902c7cfe909618f744ef91b
Glen Merto <strrev4034nelg3441@gmail.com>
2018-10-12 05:54:39 +0800

    deleted custom css

--- includes/alltime.php
+++ includes/alltime.php
@@ -12,21 +12,25 @@ $counter = 0;
 while ($row = mysqli_fetch_assoc($result)) {
     $img = $row['image'];
 
-    if ($counter % 3 == 0) {
-        echo "<div class='row'>";
+    
         echo "<div class='col-sm-4'>";
-        echo "<img src='images/{$img}' class='imgThumb'>";
+            if ($counter % 3 == 0) {
+            echo "<img src='images/{$img}' width='370px'>";
+            }
         echo "</div>";
-    } else if ($counter % 3 == 1) {
+
         echo "<div class='col-sm-4'>";
-        echo "<img src='images/{$img}' class='imgThumb'>";
+        if ($counter % 3 == 1) {
+            echo "<img src='images/{$img}' width='370px'>";
+            }
         echo "</div>";
-    } else {
-        echo "<div class='col-sm-4 mb-3'>";
-        echo "<img src='images/{$img}' class='imgThumb'>";
+
+        echo "<div class='col-sm-4'>";
+        if ($counter % 3 == 2) {
+            echo "<img src='images/{$img}' width='370px'>";
+            }
         echo "</div>";
-        echo "</div>";
-    }
+    
 
     $counter++;
 }
